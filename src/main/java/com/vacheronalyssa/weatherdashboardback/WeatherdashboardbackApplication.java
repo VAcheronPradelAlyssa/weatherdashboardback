24c763c7575c273261dc85d599ccd91cf689a1c0
Alyssa Vacheron <vacheron.alyssa@aol.com>
2026-04-06 15:22:24 +0200

    first commit

---
+++ src/main/java/com/vacheronalyssa/weatherdashboardback/WeatherdashboardbackApplication.java
@@ -0,0 +1,13 @@
+package com.vacheronalyssa.weatherdashboardback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherdashboardbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherdashboardbackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vacheronalyssa/weatherdashboardback/WeatherdashboardbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vacheronalyssa.weatherdashboardback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherdashboardbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
